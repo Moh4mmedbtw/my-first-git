fb0ba6ec4031a27b0fa5779aab1f2a473cd5c743
Mohammed <Moh4mmedbtw@gmail.comm>
2022-10-07 20:22:06 +0300

    my commit

---
+++ Sorter.java
@@ -0,0 +1,12 @@
+public class Sorter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public static boolean isSorted(int[] a){
+        System.out.println("Maybe it is");
+        return true;
+    }
+    
+}
