13934942fe1badf21fa6fc2b00325c752f857852
RakanAlomari <rakan021211@gmail.com>
2022-10-07 21:22:06 +0300

    insertionSort

--- Sorter.java
+++ Sorter.java
@@ -6,6 +6,8 @@ public class Sorter {
         int[] x = {5,4,3,1,2,7};
         selectionSort(x);
         System.out.println(Arrays.toString(x));
+        insertionSort(x);
+        System.out.println(Arrays.toString(x));
         
     }
 
@@ -27,6 +29,22 @@ public class Sorter {
             int temp = arr[min_idx];
             arr[min_idx] = arr[i];
             arr[i] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] arr){
+        int size = arr.length;
+
+        for (int step = 1; step < size; step++){
+            int key = arr[step];
+            int j = step - 1;
+
+            while (j >= 0 && key < arr[j]) {
+                arr[j + 1] = arr[j];
+                --j;
+            }
+
+            arr[j + 1] = key;
         }
     }
     
